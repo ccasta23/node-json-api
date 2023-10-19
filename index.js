4cdd7220c7fb05b57b97e2abfbef6f0eaddd6f72
Carlos Castañeda <carloscastaneda2323@gmail.com>
2023-10-18 20:57:21 -0500

    Se permite crear y eliminar mascostas desde la interfaz gráfica

--- index.js
+++ index.js
@@ -45,7 +45,51 @@ app.get('/read-file', (req, res) => {
 app.get('/pets', (req, res)=>{
     const data = readFile(FILE_NAME);
     res.render('pets/index', { pets: data });
+});
+
+//Crear Mascota
+app.get('/pets/create', (req, res)=>{
+    //Mostrar el formulario
+    res.render('pets/create');
+});
+
+app.post('/pets', (req, res) => {
+    try {
+        //Leer el archivo de mascotas
+        const data = readFile(FILE_NAME);
+        //Agregar la nueva mascota (Agregar ID)
+        const newPet = req.body;
+        newPet.id = uuidv4();
+        console.log(newPet)
+        data.push(newPet);
+        // Escribir en el archivo
+        writeFile(FILE_NAME, data);
+        res.redirect('/pets');
+    } catch (error) {
+        console.error(error);
+        res.json({ message: 'Error al almacenar la mascota' });
+    }
+});
+
+//Eliminar una mascota
+app.post('/pets/delete/:id', (req, res) => {
+    console.log(req.params.id);
+    //Guardar el ID
+    const id = req.params.id
+    //Leer el contenido del archivo
+    const pets = readFile(FILE_NAME)
+    // Buscar la mascota con el ID que recibimos
+    const petIndex = pets.findIndex(pet => pet.id === id )
+    if( petIndex < 0 ){// Si no se encuentra la mascota con ese ID
+        res.status(404).json({'ok': false, message:"Pet not found"});
+        return;
+    }
+    //Eliminar la mascota que esté en la posición petIndex
+    pets.splice(petIndex, 1);
+    writeFile(FILE_NAME, pets)
+    res.redirect('/pets');
 })
+
 
 // API
 // Listar Mascotas
